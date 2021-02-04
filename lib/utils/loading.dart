32dc8aca16de0b1d4c85355ca11cca54100892fd
makoto yasuda <>
2021-02-05 00:18:18 +0900

    修正

--- lib/utils/loading.dart
+++ lib/utils/loading.dart
@@ -5,7 +5,7 @@ class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SpinKitPouringHourglass(
+      body: SpinKitFadingCircle(
         color: Colors.lightBlue,
         size: 200.0,
       ),
