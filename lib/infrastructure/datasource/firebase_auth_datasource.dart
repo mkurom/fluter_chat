41a45869fbb5f4d8f927f1a6bcee325fb68c958e
makoto yasuda <>
2021-02-10 00:27:19 +0900

    fix

--- lib/infrastructure/datasource/firebase_auth_datasource.dart
+++ lib/infrastructure/datasource/firebase_auth_datasource.dart
@@ -2,29 +2,24 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseAuthDatasource {
-  String _uid = "";
+  // String _uid = "";
 
   Future<FirebaseApp> initialize() {
     print("initialize");
 
-    return Firebase.initializeApp().catchError((err) => {
-          print(err.toString()),
-        });
+    return Firebase.initializeApp().then((value) async {
+      await signInAnonymous();
+    }).catchError(
+      (err) => {
+        print(err.toString()),
+      },
+    );
   }
 
-  Future<String> signInAnonymous() async {
+  Future<void> signInAnonymous() async {
     print("signInAnonymous");
-    String errWord = "";
     var task = await FirebaseAuth.instance.signInAnonymously();
 
-    _uid = task.user.uid;
-
     print(task.user.uid);
-
-    if (task.user.uid == null) {
-      errWord = "not user";
-    }
-
-    return errWord;
   }
 }
